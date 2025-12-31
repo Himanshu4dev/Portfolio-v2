8b38327ed4da38b9a5d7e66d40fc2f2598b82d11
Himanshu <hp370081@gmail.com>
2025-12-31 14:38:11 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,6 +22,7 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${spaceGrotesk.variable} ${playfairDisplay.variable} font-body antialiased bg-background text-foreground`}
+        suppressHydrationWarning
       >
         {children}
         <Analytics />
